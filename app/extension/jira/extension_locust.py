f1d9d0b9c707f47bdcf8efec70c7d67d8e4dbad1
Pavel Karalenak <pavel.k@polontech.biz>
2022-03-14 15:43:58 +0300

    [SC-1033] Add Smart Checklist locust tests

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -1,29 +1,68 @@
+import random
 import re
-from locustio.common_utils import init_logger, jira_measure, run_as_specific_user  # noqa F401
+from locustio.common_utils import init_logger, jira_measure, generate_random_string, TEXT_HEADERS
+from locustio.jira.requests_params import jira_datasets
 
 logger = init_logger(app_type='jira')
-
+jira_dataset = jira_datasets()
+# Smart Checklist app configuration
+status_ids = [1, 2, 3, 4]
 
 @jira_measure("locust_app_specific_action")
-# @run_as_specific_user(username='admin', password='admin')  # run as specific user
 def app_specific_action(locust):
-    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
-    content = r.content.decode('utf-8')   # decode response content
-
-    token_pattern_example = '"token":"(.+?)"'
-    id_pattern_example = '"id":"(.+?)"'
-    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
-
-    logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in jira.yml file
-    if 'assertion string' not in content:
-        logger.error(f"'assertion string' was not found in {content}")
-    assert 'assertion string' in content  # assert specific string in response content
-
-    body = {"id": id, "token": token}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
-    content = r.content.decode('utf-8')
-    if 'assertion string after successful POST request' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'assertion string after successful POST request' in content  # assertion after POST request
+    # update locustio.jira.requests_params with 'data_sets["custom-issues"] = read_input_file
+(JIRA_DATASET_CUSTOM_ISSUES)'
+    issue_key = random.choice(jira_dataset["custom-issues"])[0]
+    logger.locust_info(f"Get associated checklists from {issue_key}")
+    CHECKLIST_PARAMS = {
+        "issueKey": issue_key
+    }
+    #1
+    checklist_request = locust.get("/rest/railsware/1.0/checklist", params=CHECKLIST_PARAMS,
+catch_response=True)
+    checklist_content = checklist_request.content.decode("utf-8")
+    if "checklists" not in checklist_content:
+        logger.error(f"'checklists' was not found in {checklist_content}")
+    checklist_id_pattern = '"checklistId":(.[0-9]+)'
+    checklist_ids = re.findall(checklist_id_pattern, checklist_content)
+    if len(checklist_ids) > 0:
+        checklist_id = checklist_ids[0]
+        logger.locust_info(f"Check list id: {checklist_id}")
+        item_id_pattern = '"id":([0-9]+)'
+        item_ids = re.findall(item_id_pattern, checklist_content)
+        TEXT_HEADERS["Content-Type"] = "application/json"
+        label = generate_random_string(20, only_letters=True)
+        if len(item_ids) > 0:
+            item_id = item_ids[0]
+            logger.locust_info(f"Item id: {item_id}")
+            logger.locust_info(f"Updating checklist item in {issue_key}")
+            status_id = random.choice(status_ids)
+            update_checklist_body = []
+            item_for_update = {
+                "id": item_id,
+                "label": label,
+                "rank": 1,
+                "status": {
+                    "id": status_id
+                }
+            }
+            update_checklist_body.append(item_for_update)
+            #2
+            update_item_request = locust.client.request(method="PUT", url=f"/rest/railsware/1.0/checklist/
+{checklist_id}", json=update_checklist_body, headers=TEXT_HEADERS, catch_response=True)
+            update_item_content = update_item_request.content.decode("utf-8")
+            if label not in update_item_content:
+                logger.error(f"Label {label} was not found in {update_item_content}")
+        else:
+            logger.locust_info(f"Creating checklist item in {issue_key}")
+            #3
+            label = "- " + label
+            item_body = {
+                "isReplace": "false",
+                "stringValue": label
+            }
+            create_item_request = locust.client.request(method="PUT", url=f"/rest/railsware/1.0/checklist/
+{checklist_id}/item", json=item_body, headers=TEXT_HEADERS, catch_response=True)
+            create_item_content = create_item_request.content.decode("utf-8")
+            if "checklists" not in create_item_content:
+                logger.error(f"'checklists' was not found in {create_item_content}")
